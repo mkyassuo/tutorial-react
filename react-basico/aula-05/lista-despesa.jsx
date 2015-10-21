850cf2420e10ffc95f65d43caad48c9d8033a4d6
mkyassuo <mkyassuo@yahoo.com.br>
2015-10-21 17:32:59 -0200

    modal arrumado

--- react-basico/aula-05/lista-despesa.jsx
+++ react-basico/aula-05/lista-despesa.jsx
@@ -16,27 +16,25 @@ class ListaDespesa extends React.Component{
 		}
 	}
 
-	_onClickRemover(index, excluir)	{
+	_onClickRemover(index)	{
 		if (!this.props.onRemoverDespesa)
 		{
 			return
 		}
-		if (!this.state.showModal)
-		{
-			this.setState({
-				showModal: true,
-				index: index
-			})
-			return
-		}
-		if (excluir){
-			this.props.onRemoverDespesa(this.state.index);
-		}
+		this.props.onRemoverDespesa(this.state.index);
 		this.setState({
 			showModal: false,
 			index: null
-		})		
+		})
 	}
+
+	_modalExcluir(index, modal){
+		this.setState({
+			showModal: modal,
+			index: index
+		})
+	}
+
 
 	_closeModal(){
 		this.setState({
@@ -89,6 +87,7 @@ class ListaDespesa extends React.Component{
 		});
 	}
 
+
   render(){
       var total = this.props.despesas.reduce(function(xpto, item){
         return  xpto + parseInt(item.valor)
@@ -96,18 +95,22 @@ class ListaDespesa extends React.Component{
       return(
 
         <div>
-					<Modal show={this.state.showModal} onHide={() => this._closeModal()}>
-					  <Modal.Header closeButton>
-					    <Modal.Title>Excluir Item</Modal.Title>
-					  </Modal.Header>
-					  <Modal.Body>
-					    <p>Voce tem certeza que deseja excluir este item?</p>
-					  </Modal.Body>
-					  <Modal.Footer>
-					    <Button onClick={()=>this._onClickRemover(this.state.index,true)}>Sim</Button>
-							<Button onClick={()=>this._onClickRemover(this.state.index,false)}>Não</Button>
-					  </Modal.Footer>
-					</Modal>
+					{!this.state.showModal ?
+						 null :
+						 <Modal show={this.state.showModal} onHide={() => this._closeModal()}>
+							 <Modal.Header closeButton>
+								 <Modal.Title>Excluir Item</Modal.Title>
+							 </Modal.Header>
+							 <Modal.Body>
+								 <p>Voce tem certeza que deseja excluir este item?</p>
+							 </Modal.Body>
+							 <Modal.Footer>
+								 <Button onClick={()=>this._onClickRemover(this.state.index)}>Sim</Button>
+								 <Button onClick={()=>this._modalExcluir(null, false)}>Não</Button>
+							 </Modal.Footer>
+						 </Modal>
+					}
+
 				<div className='row'>
 					<Label className='col-md-2 col-xs-12'>Todos os Itens</Label>
 				</div>
@@ -130,7 +133,7 @@ class ListaDespesa extends React.Component{
 						return <div key={index} className='row'>
 							<span className= 'col-md-2 col-xs-12'>{`${item.descricao}`}</span>
 							<span className= 'col-md-2 col-xs-12'>{`${item.valor}`}</span>
-							<Button className= 'col-md-2 col-xs-12' onClick={() => this._onClickRemover(index)}>Remover</Button>
+							<Button className= 'col-md-2 col-xs-12' onClick={() => this._modalExcluir(index, true)}>Remover</Button>
 							<Button className= 'col-md-2 col-xs-12' onClick={() => this._editarDespesa(index)}>Alterar</Button>
 						</div>
 					})}
